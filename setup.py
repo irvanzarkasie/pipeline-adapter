fbed42bdc8b75afcc53908f289b59dc8da66b1ac
irvanzarkasie <irvanzarkasie@gmail.com>
2019-12-23 14:51:08 +0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name="pipeline-adapter",
+    version="0.0.1",
+    author="expecc",
+    author_email="expecc@expecc.com",
+    description="Common package for executing pipeline",
+    packages=setuptools.find_packages(),
+    include_package_data=True
+)
